f6d4db9b41e6b84680eb8582bdd3727ca001b085
hews <p@hews.co>
2015-08-12 02:37:14 -0700

    add checksum

--- git-grab.rb
+++ git-grab.rb
@@ -4,7 +4,7 @@ class GitGrab < Formula
   homepage "https://github.com/h4w5/git-grab"
   url "https://github.com/h4w5/git-grab/archive/v0.2.0.tar.gz"
   version "0.2.0"
-  sha256 ""
+  sha256 "1ae66eac8887bcb5c071e344dc37e78d749cd1715bb8e883cbec75128d058fb0"
 
   depends_on "svn"
 
