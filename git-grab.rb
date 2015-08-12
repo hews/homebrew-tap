e983dad3f446128ad14a6ae31709dd99d15e7d28
hews <p@hews.co>
2015-08-12 02:35:45 -0700

    update

--- git-grab.rb
+++ git-grab.rb
@@ -2,14 +2,18 @@ class GitGrab < Formula
   desc "A simple shell script that allows a user to grab and download a " + \
        "sub-directory from a Git repo."
   homepage "https://github.com/h4w5/git-grab"
-  url "https://github.com/h4w5/git-grab/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "49f9dac555aaba1ae1050b5c8c81b7837ee7a3611385ef312c15bed63da3a352"
+  url "https://github.com/h4w5/git-grab/archive/v0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 ""
 
   depends_on "svn"
 
   def install
     bin.install "git-grab"
     bin.install "git-ls-sub"
+  end
+
+  test do
+    system "git grab", "https://github.com/h4w5/git-grab/blob/master/README.md"
   end
 end
