213bbd04ba09b3f23868f9ad6f4bb9b35ea28715
hews <p@hews.co>
2015-08-22 16:06:23 -0700

    clean up

--- git-grab.rb
+++ git-grab.rb
@@ -1,10 +1,11 @@
 class GitGrab < Formula
   desc "A simple shell script that allows a user to grab and download a " + \
-       "sub-directory from a Git repo."
+       "sub-directory or file from a Git repo."
+
   homepage "https://github.com/h4w5/git-grab"
-  url "https://github.com/h4w5/git-grab/archive/v0.2.0.tar.gz"
-  version "0.2.0"
-  sha256 "1ae66eac8887bcb5c071e344dc37e78d749cd1715bb8e883cbec75128d058fb0"
+  url      "https://github.com/h4w5/git-grab/archive/v0.2.0.tar.gz"
+  version  "0.2.0"
+  sha256   "1ae66eac8887bcb5c071e344dc37e78d749cd1715bb8e883cbec75128d058fb0"
 
   depends_on "svn"
 
