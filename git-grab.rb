f01766cbcbcab1a122aff1b75a8253f307eac7aa
Philip Hughes <hews@users.noreply.github.com>
2016-06-13 17:05:44 -0700

    Update git-grab to point to new repo

--- git-grab.rb
+++ git-grab.rb
@@ -2,10 +2,10 @@ class GitGrab < Formula
   desc "A simple shell script that allows a user to grab and download a " + \
        "sub-directory or file from a Git repo."
 
-  homepage "https://github.com/h4w5/git-grab"
-  url      "https://github.com/h4w5/git-grab/archive/v0.2.0.tar.gz"
+  homepage "https://github.com/hews/git-grab"
+  url      "https://github.com/hews/git-grab/archive/v0.2.0.tar.gz"
   version  "0.2.0"
-  sha256   "1ae66eac8887bcb5c071e344dc37e78d749cd1715bb8e883cbec75128d058fb0"
+  sha256   "d1eca448f6a64b30acff745fa96f1a15cf3a6d14c1c726c18eb63ab37053cd1d"
 
   depends_on "svn"
 
@@ -15,6 +15,6 @@ class GitGrab < Formula
   end
 
   test do
-    system "git grab", "https://github.com/h4w5/git-grab/blob/master/README.md"
+    system "git grab", "https://github.com/hews/git-grab/blob/master/README.md"
   end
 end
