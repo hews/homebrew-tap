92e8264d1f57f2bd35a052164438165dbe9760b8
Philip Hughes <hews@users.noreply.github.com>
2016-12-06 10:01:58 -0800

    Update git-grab.rb

--- git-grab.rb
+++ git-grab.rb
@@ -3,9 +3,9 @@ class GitGrab < Formula
        "sub-directory or file from a Git repo."
 
   homepage "https://github.com/hews/git-grab"
-  url      "https://github.com/hews/git-grab/archive/v0.2.0.tar.gz"
-  version  "0.2.0"
-  sha256   "d1eca448f6a64b30acff745fa96f1a15cf3a6d14c1c726c18eb63ab37053cd1d"
+  url      "https://github.com/hews/git-grab/archive/v0.2.1.tar.gz"
+  version  "0.2.1"
+  sha256   "e5e4eb664b15129e427030b5578f1a710ccb639a306f4e4912026ec17396bbb1"
 
   depends_on "svn"
 
