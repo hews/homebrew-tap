6c5c13b5e71685dbdef351650b258ca36fa9ca5d
Philip Hughes <hews@users.noreply.github.com>
2016-12-06 10:11:52 -0800

    Update git-grab.rb

--- git-grab.rb
+++ git-grab.rb
@@ -5,7 +5,7 @@ class GitGrab < Formula
   homepage "https://github.com/hews/git-grab"
   url      "https://github.com/hews/git-grab/archive/v0.2.1.tar.gz"
   version  "0.2.1"
-  sha256   "e5e4eb664b15129e427030b5578f1a710ccb639a306f4e4912026ec17396bbb1"
+  sha256   "70e23c74a894ea913de0f190d669c85265518eb4ad79a8e363c2a5666b9abf87"
 
   depends_on "svn"
 
